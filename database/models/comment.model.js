419e2b7abce5b3cb924df045bf64595a0fccff10
Mostafa Ragab <m.hassan323@alexu.edu.eg>
2023-10-07 21:20:46 +0300

    comment model init

---
+++ database/models/comment.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: [1, "Comment text must be at least 1 character"],
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
